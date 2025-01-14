23863fed628bfb858681c56c3dac6b0e66a56ac7
Alan <131834711+lanxin1216@users.noreply.github.com>
2025-01-14 20:03:06 +0800

    - 删除图片

--- src/main/java/com/alan/alanpicturebackend/controller/PictureController.java
+++ src/main/java/com/alan/alanpicturebackend/controller/PictureController.java
@@ -2,18 +2,19 @@ package com.alan.alanpicturebackend.controller;
 
 import com.alan.alanpicturebackend.annotation.AuthCheck;
 import com.alan.alanpicturebackend.common.BaseResponse;
+import com.alan.alanpicturebackend.common.DeleteRequest;
 import com.alan.alanpicturebackend.common.ResultUtils;
 import com.alan.alanpicturebackend.constant.UserConstant;
+import com.alan.alanpicturebackend.exception.BusinessException;
+import com.alan.alanpicturebackend.exception.ErrorCode;
+import com.alan.alanpicturebackend.exception.ThrowUtils;
 import com.alan.alanpicturebackend.model.dto.picture.PictureUploadRequest;
+import com.alan.alanpicturebackend.model.entity.Picture;
 import com.alan.alanpicturebackend.model.entity.User;
 import com.alan.alanpicturebackend.model.vo.PictureVO;
 import com.alan.alanpicturebackend.service.PictureService;
 import com.alan.alanpicturebackend.service.UserService;
-import org.springframework.beans.BeanUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
@@ -49,5 +50,32 @@ public class PictureController {
         User loginUser = userService.getLoginUser(httpServletRequest);
         PictureVO pictureVO = pictureService.uploadPicture(multipartFile, pictureUploadRequest, loginUser);
         return ResultUtils.success(pictureVO);
+    }
+
+    /**
+     * 删除图片
+     *
+     * @param deleteRequest 删除请求
+     * @return 删除结果
+     */
+    @PostMapping("/delete")
+    public BaseResponse<Boolean> deletePicture(@RequestBody DeleteRequest deleteRequest, HttpServletRequest request) {
+        if (deleteRequest == null || deleteRequest.getId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 获取用户
+        User loginUser = userService.getLoginUser(request);
+        Long id = deleteRequest.getId();
+        // 判断图片是否存在
+        Picture oldPicture = pictureService.getById(id);
+        ThrowUtils.throwIf(oldPicture == null, ErrorCode.NOT_FOUND_ERROR);
+        // 权限判断，本人或管理员
+        if (!oldPicture.getUserId().equals(loginUser.getId()) && !userService.isAdmin(loginUser)) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+        // 操作数据库
+        boolean result = pictureService.removeById(id);
+        ThrowUtils.throwIf(!result, ErrorCode.SYSTEM_ERROR, "图片删除异常");
+        return ResultUtils.success(true);
     }
 }
