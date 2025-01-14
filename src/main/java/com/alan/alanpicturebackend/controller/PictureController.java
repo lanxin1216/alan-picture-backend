c02c6e15cd802691d936b0cc848fd2b881c25c9e
Alan <131834711+lanxin1216@users.noreply.github.com>
2025-01-14 20:36:32 +0800

    - 获取图片信息（用户、管理员） - 获取图片分页列表（用户、管理员）

--- src/main/java/com/alan/alanpicturebackend/controller/PictureController.java
+++ src/main/java/com/alan/alanpicturebackend/controller/PictureController.java
@@ -4,11 +4,13 @@ import cn.hutool.json.JSONUtil;
 import com.alan.alanpicturebackend.annotation.AuthCheck;
 import com.alan.alanpicturebackend.common.BaseResponse;
 import com.alan.alanpicturebackend.common.DeleteRequest;
+import com.alan.alanpicturebackend.common.PageRequest;
 import com.alan.alanpicturebackend.common.ResultUtils;
 import com.alan.alanpicturebackend.constant.UserConstant;
 import com.alan.alanpicturebackend.exception.BusinessException;
 import com.alan.alanpicturebackend.exception.ErrorCode;
 import com.alan.alanpicturebackend.exception.ThrowUtils;
+import com.alan.alanpicturebackend.model.dto.picture.PictureQueryRequest;
 import com.alan.alanpicturebackend.model.dto.picture.PictureUpdateRequest;
 import com.alan.alanpicturebackend.model.dto.picture.PictureUploadRequest;
 import com.alan.alanpicturebackend.model.entity.Picture;
@@ -16,6 +18,8 @@ import com.alan.alanpicturebackend.model.entity.User;
 import com.alan.alanpicturebackend.model.vo.PictureVO;
 import com.alan.alanpicturebackend.service.PictureService;
 import com.alan.alanpicturebackend.service.UserService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -109,5 +113,81 @@ public class PictureController {
         boolean result = pictureService.updateById(picture);
         ThrowUtils.throwIf(!result, ErrorCode.SYSTEM_ERROR, "更新图片错误");
         return ResultUtils.success(true);
+    }
+
+    /**
+     * 根据 id 获取图片（仅管理员）
+     *
+     * @param id      图片 id
+     * @param request 请求
+     * @return 返回图片详细信息
+     */
+    @GetMapping("/get")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<Picture> getPictureById(long id, HttpServletRequest request) {
+        ThrowUtils.throwIf(id <= 0, ErrorCode.PARAMS_ERROR);
+        // 查询数据
+        Picture picture = pictureService.getById(id);
+        ThrowUtils.throwIf(picture == null, ErrorCode.NOT_FOUND_ERROR);
+        // 返回
+        return ResultUtils.success(picture);
+    }
+
+    /**
+     * 根据 id 获取图片（封装类）
+     *
+     * @param id      图片 id
+     * @param request 请求
+     * @return 返回图片详细信息
+     */
+    @GetMapping("/get/vo")
+    public BaseResponse<PictureVO> getPictureVOById(long id, HttpServletRequest request) {
+        ThrowUtils.throwIf(id <= 0, ErrorCode.PARAMS_ERROR);
+        // 查询数据
+        Picture picture = pictureService.getById(id);
+        ThrowUtils.throwIf(picture == null, ErrorCode.NOT_FOUND_ERROR);
+        // 封装数据
+        PictureVO pictureVO = pictureService.getPictureVO(picture, request);
+        // 返回
+        return ResultUtils.success(pictureVO);
+    }
+
+    /**
+     * 获取图片分页列表（仅管理员）
+     *
+     * @param pictureQueryRequest 分页请求
+     * @return 返回详细分页列表
+     */
+    @GetMapping("/list/page")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<Page<Picture>> listPictureByPage(@RequestBody PictureQueryRequest pictureQueryRequest) {
+        long current = pictureQueryRequest.getCurrent();
+        long size = pictureQueryRequest.getPageSize();
+        // 查询
+        Page<Picture> objectPage = new Page<>(current, size);
+        QueryWrapper<Picture> queryWrapper = pictureService.getQueryWrapper(pictureQueryRequest);
+        Page<Picture> picturePage = pictureService.page(objectPage, queryWrapper);
+        return ResultUtils.success(picturePage);
+    }
+
+    /**
+     * 获取图片分页列表（封装类）
+     *
+     * @param pictureQueryRequest 分页请求
+     * @return 返回详细分页 VO 列表
+     */
+    @GetMapping("/list/page/vo")
+    public BaseResponse<Page<PictureVO>> listPictureVOByPage(@RequestBody PictureQueryRequest pictureQueryRequest, HttpServletRequest request) {
+        long current = pictureQueryRequest.getCurrent();
+        long size = pictureQueryRequest.getPageSize();
+        // 限制查询条数最大值，防止爬虫
+        ThrowUtils.throwIf(size > 20, ErrorCode.PARAMS_ERROR);
+        // 查询
+        Page<Picture> objectPage = new Page<>(current, size);
+        QueryWrapper<Picture> queryWrapper = pictureService.getQueryWrapper(pictureQueryRequest);
+        Page<Picture> picturePage = pictureService.page(objectPage, queryWrapper);
+        // 封装
+        Page<PictureVO> pictureVOPage = pictureService.getPictureVOPage(picturePage, request);
+        return ResultUtils.success(pictureVOPage);
     }
 }
